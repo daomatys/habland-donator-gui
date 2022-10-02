856b9467040e9581ff2807bc4a71ae82d95a46c0
Maxim Dejnew <red41tiger@mail.ru>
2022-10-02 18:36:40 +0700

    Edit script

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name          habland-donator-gui
 // @namespace     https://github.com/daomatys/habland-donator-gui
-// @version       0.1
 // @description   Simple overlay for habland donator's privileges
+// @icon          https://sun4-17.userapi.com/oBEu9jqFA2acYIE4CRhcZT_XAiYqOlIzj9j8Yg/E2hRXabLipU.png?6
+// @version       0.1
 // @author        daomatys
 // @match         https://vk.com/app4315377_142573694
 // @grant         none
