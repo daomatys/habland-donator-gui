440626a01dc44e51e9ff4f971174e9018ea49619
Maxim Dejnew <red41tiger@mail.ru>
2022-10-02 18:28:51 +0700

    Added a script

---
+++ index.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          habland-donator-gui
+// @namespace     https://github.com/daomatys/habland-donator-gui
+// @version       0.1
+// @description   Simple overlay for habland donator's privileges
+// @author        daomatys
+// @match         https://vk.com/app4315377_142573694
+// @grant         none
+// ==/UserScript==
+
+(function () {
+  console.log('hello, world');
+})();
