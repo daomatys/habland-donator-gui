4f8a6b49cd5dc009cbbf10a5770720da508a38f9
Maxim Dejnew <red41tiger@mail.ru>
2022-10-11 22:35:01 +0700

    Commit

---
+++ index.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name          habland-donator-gui
+// @namespace     https://github.com/daomatys/habland-donator-gui
+// @description   Simple overlay for habland donator's privileges
+// @icon          https://sun4-17.userapi.com/oBEu9jqFA2acYIE4CRhcZT_XAiYqOlIzj9j8Yg/E2hRXabLipU.png?6
+// @version       0.1
+// @author        daomatys
+// @match         https://vk.com/app4315377_142573694
+// @grant         none
+// ==/UserScript==
+
+(function () {
+  const inputSelector = '.chat-input';
+
+  console.log(inputSelector);
+  
+  const markup = (`
+    <div class="sidebar">
+      <div class="sidebar__background" id="sidebar">
+        hello
+      </div>
+    </div>`
+  );
+
+  document.addEventListener('DOMContentLoaded', () => {
+    document.insertAdjacentHTML('afterbegin', markup);
+    console.log(markup);
+  });
+  
+})();
